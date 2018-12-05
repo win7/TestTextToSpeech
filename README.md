bb3e7671b013d1a740bd40c3a827992288f17e4b
win7 <win7.eam@gmail.com>
2018-12-05 17:08:30 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TestTextToSpeech
---
+++ app/src/main/java/com/go/win7/testtexttospeech/MainActivity.java
@@ -0,0 +1,88 @@
+package com.go.win7.testtexttospeech;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et_text;
+    TextToSpeech text_to_speeach;
+
+    String TAG = "GOOO Main";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        et_text = findViewById(R.id.editText_text);
+        text_to_speeach = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status == TextToSpeech.SUCCESS){
+                    text_to_speeach.setLanguage(Locale.getDefault());
+                } else {
+                    Log.e("", "Initialization failed");
+                }
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /* Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show(); */
+                text_to_speeach.setSpeechRate(0.8f);
+                int speechStatus = text_to_speeach.speak(et_text.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+                if (speechStatus == TextToSpeech.ERROR) {
+                    Log.e("", "Error in converting Text to Speech!");
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (text_to_speeach != null) {
+            text_to_speeach.stop();
+            text_to_speeach.shutdown();
+        }
+    }
+}
